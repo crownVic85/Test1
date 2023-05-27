4ce3a7d3e4cc8776f92fc7ea028d9fdf795088c2
crownVic85 <victor0285@yahoo.com>
2023-05-27 10:59:59 -0700

    class vehicle added

---
+++ Car OO Challenge/car OO.js
@@ -0,0 +1,10 @@
+class Vehicle {
+    constructor(make,model, year) {
+        this,make = make;
+        this.model = model;
+        this.year = year;
+    }
+    honk() {
+        return "Beep";
+    }
+}
